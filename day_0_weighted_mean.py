d9e1914ee487d0e4b672701312781866bff30183
Jonatas Fontele <jonatasfontele@gmail.com>
2021-05-26 21:49:57 -0300

    Day 0 challenge
    Weighted Mean function with list comprehension and zip or with numpy.

---
+++ day_0_weighted_mean.py
@@ -0,0 +1,19 @@
+# import numpy as np
+
+
+def weighted_mean(x, w):
+    # Zip() returns a list of tuples, where an i-th tuple contains the i-th element of each of the arguments.
+    return round(sum(val * weight for val, weight in zip(x, w)) / sum(w), 1)
+    # or
+    # return round(np.average([x], weights=[w]), 1)
+
+
+def main():
+    n = int(input())
+    vals = list(map(int, input().split()))
+    weights = list(map(int, input().split()))
+    print(weighted_mean(vals, weights))
+
+
+if __name__ == "__main__":
+    main()
