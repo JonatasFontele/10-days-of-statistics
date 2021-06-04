3aed04ecad74ed91ec98b6e59511c0c05f1841b7
Jonatas Fontele <jonatasfontele@gmail.com>
2021-06-03 21:05:14 -0300

    Day 1 challenge
    Proper import

--- day_1_standard_deviation.py
+++ day_1_standard_deviation.py
@@ -1,9 +1,10 @@
 from math import sqrt
-import statistics
+from statistics import mean
 
 
 def std_dev(arr):
-    return round(sqrt(sum((val - statistics.mean(arr))**2 for val in arr) / len(arr)), 1)
+    average = mean(arr)
+    return round(sqrt(sum((val - average) ** 2 for val in arr) / len(arr)), 1)
     # or
     # return round(statistics.pstdev(arr), 1)
     # pstdev is population standard deviation, while stdev is sample standard deviation len(arr)-1
