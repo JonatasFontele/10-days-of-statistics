79fd2aa4bd4ebe367b7d18a851e6c4833a730618
Jonatas Fontele <jonatasfontele@gmail.com>
2021-07-04 23:41:47 -0300

    Add files via upload

---
+++ day_3_cards_of_the_same_suit.py
@@ -0,0 +1,22 @@
+from itertools import product
+from fractions import Fraction
+from random import choice
+
+
+# Draw 2 cards from a 52-card deck without replacing. What is the probability that both cards are of the same suit?
+def calculate_draw_2_cards_prob():
+    pass
+
+
+def draw_2_cards(n_simulations=100000):
+    pass
+
+
+def main():
+    print(calculate_draw_2_cards_prob())
+    print(draw_2_cards())
+    print(round(draw_2_cards()*100, 2), "%")
+
+
+if __name__ == "__main__":
+    main()
